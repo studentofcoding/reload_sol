3384a3f2ea0857ec9a553be22e54fbcf068a4b49
Yonathan Evan Christy <yonathanevan@gmail.com>
2025-03-16 11:52:29 +0700

    Add devwallet

--- src/config/devWallets.ts
+++ src/config/devWallets.ts
@@ -1,7 +1,8 @@
 export const devWallets = [
   // Add your dev wallet addresses here
   '3V3N5xh6vUUVU3CnbjMAXoyXendfXzXYKzTVEsFrLkgX',
-  'FrKWJKLDvTn4ghZSkMXTSERPA7KowWLds2vsMw7QpvNk'
+  'FrKWJKLDvTn4ghZSkMXTSERPA7KowWLds2vsMw7QpvNk',
+  '2KbA4Z1twQCYZj4MNvX5RKNKh8vWGpiQbGBPcAjtBpYS'
 ];
 
 export const isDevWallet = (address: string): boolean => {
